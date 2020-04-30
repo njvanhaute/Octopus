a29287859781a834a78b6a5258f8976a06d762aa
Nicholas Vanhaute <njvanhaute@crimson.ua.edu>
2020-04-30 16:23:28 -0500

    Initial Commit

---
+++ Octopus/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Octopus
+//
+//  Created by Nicholas Vanhaute on 4/30/20.
+//  Copyright © 2020 Nicholas Vanhaute. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
